f7552164d2cb5b5bb3e21c9925ff62fe5d39425c
Mike-Nyambe <marklinenyambe@gmail.com>
2024-05-13 14:47:34 +0200

    mike

---
+++ src/utils/data.ts
@@ -0,0 +1,45 @@
+const services = [
+  {
+    title: "Water Treatment",
+    description:
+      "Under the water treatment division, we provide water solutions to our clientele through treatment, monitoring, conditioning and technical consultancy",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+  },
+  {
+    title: "Supply Water Treatment Products",
+    description:
+      " satisfy our clientele through product and process innovation, cost reduction activities, preventive action, and client requirement specification.",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+  },
+  {
+    title: "Mining and Mineral Processing Industries",
+    description: `steel pipes and fittings, PVC pipes & fittings,Pumps- all types including spares and seals Valves,Measuring and testing instruments`,
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+  },
+  {
+    title: "Supplying Auto Spares",
+    description:
+      "We are specialized in the supply of auto spares on small or large scale and to all industries that need auto spares.",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+  },
+  {
+    title: "Water treatment chemicals & waste treatment",
+    description:
+      "We also install and supply Filtration cartridges and systems, Chemicals dosing systems",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+  },
+  {
+    title: "Industrial Equipment Supply",
+    description:
+      "We supply Industrial valves, pumps, HDPE pipes, machenical parts, Effluent palnts, filter media resins,portal water palnts and many more.",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+  },
+];
+
+export { services };
