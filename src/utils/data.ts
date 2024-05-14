8b98f352e79d4b2a6cc9ccba9073dc00f7ccdd04
Mike-Nyambe <marklinenyambe@gmail.com>
2024-05-14 14:15:41 +0200

    mik

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,33 +1,33 @@
 const services = [
   {
-    title: "Electrical engineering",
+    title: "Water Treatment Themicals",
     description:
       "Under the water treatment division, we provide water solutions to our clientele through treatment, monitoring, conditioning and technical consultancy",
     icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
   },
   {
-    title: "Civil construction",
+    title: "Civil Construction",
     description:
       "Civil construction involves planning, designing, constructing, and maintaining infrastructure like roads, bridges, dams, and buildings for societal development.",
     icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
   },
   {
-    title: "Mechanical engineering",
+    title: "Mechanical Engineering",
     description: `Mechanical engineering as a service entails providing expertise in design, analysis, and optimization of mechanical systems for various industries' needs.`,
     icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
   },
   {
-    title: "Electrical engineering",
+    title: "Electrical Engineering",
     description:
       "Electrical engineering as a service involves offering expertise in designing, implementing, and maintaining electrical systems for various applications, industries, and projects.",
     icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
   },
   {
-    title: "Renewable energy",
+    title: "Renewable Energy",
     description:
       "Renewable energy as a service involves providing consulting, design, installation, and maintenance of renewable energy systems like solar, wind, and hydroelectric for businesses and communities.",
     icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
